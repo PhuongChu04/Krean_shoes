eabf027c04e7b19c37ff85e178814423088cdf4e
nauqdz <minhquan152535@gmail.com>
2026-05-07 20:32:34 +0700

    Sửa icon cho điều hướng trang đánh giá

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -153,9 +153,9 @@
              <li class="nav-item">
                  <a class="nav-link" href="{{ route('admin.review') }}">
                      <span class="nav-icon">
-                         <iconify-icon icon="solar:chat-round-bold-duotone"></iconify-icon>
+                         <iconify-icon icon="solar:star-bold-duotone"></iconify-icon>
                      </span>
-                     <span class="nav-text"> Bình luận </span>
+                     <span class="nav-text"> Đánh giá </span>
                  </a>
              </li>
              <li class="nav-item">
