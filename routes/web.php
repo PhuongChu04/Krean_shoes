3cd2f95f71bcc436ef0f675d5d2ecb0a73ff357d
tultph39236-blip <tultph39236@gmail.com>
2026-05-07 23:09:56 +0700

    Tính năng ẩn hiện bình luận

--- routes/web.php
+++ routes/web.php
@@ -296,6 +296,8 @@ Route::prefix('admin')->name('admin.')->middleware('checkAdmin')->group(function
 
     Route::prefix('comments')->name('comments.')->group(function () {
         Route::get('/', [CommentController::class, 'index'])->name('index');
+        Route::post('/hide', [CommentController::class, 'hide'])->name('hide');
+        Route::get('/{id}', [CommentController::class, 'show'])->name('show');
         Route::post('/show-again', [CommentController::class, 'showAgain'])->name('showAgain');
     });
 });
