6391d33688d39f07c88164f8971278ed9580b82f
tultph39236-blip <tultph39236@gmail.com>
2026-05-07 23:15:09 +0700

    Sửa lỗi điều hướng trang danh sách bình luận

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -151,7 +151,7 @@
                  </a>
              </li>
              <li class="nav-item">
-                 <a class="nav-link" href="{{ route('admin.review') }}">
+                 <a class="nav-link" href="{{ route('admin.comments.index') }}">
                      <span class="nav-icon">
                          <iconify-icon icon="solar:chat-round-bold-duotone"></iconify-icon>
                      </span>
